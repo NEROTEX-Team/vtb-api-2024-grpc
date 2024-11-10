55c0c43dc7f0593cc80d89b65d23039ac8fc753e
Danila Kutuzov <danilakutuzov@gmail.com>
2024-11-10 15:23:32 +0300

    Update antivirus service

--- antivirus/scanner.go
+++ antivirus/scanner.go
@@ -10,34 +10,40 @@ import (
 )
 
 type Scanner struct {
-	address string
-	network string
-	timeout time.Duration
+	address      string
+	network      string
+	timeout      time.Duration
+	useAntivirus bool
 }
 
-func NewScanner(address, network string, timeout time.Duration) *Scanner {
+func NewScanner(address, network string, timeout time.Duration, useAntivirus bool) *Scanner {
 	return &Scanner{
-		address: address,
-		network: network,
-		timeout: timeout,
+		address:      address,
+		network:      network,
+		timeout:      timeout,
+		useAntivirus: useAntivirus,
 	}
 }
 
 func (s *Scanner) ScanFile(filePath string) error {
+	if !s.useAntivirus {
+		return nil
+	}
+
 	conn, err := net.DialTimeout(s.network, s.address, s.timeout)
 	if err != nil {
-		return fmt.Errorf("Ошибка соединения с антивирус сервисом: %w", err)
+		return fmt.Errorf("Ошибка соединения сервиса антивируса: %w", err)
 	}
 	defer conn.Close()
 
 	_, err = fmt.Fprintf(conn, "INSTREAM\n")
 	if err != nil {
-		return fmt.Errorf("Ошибка отправки INSTREAM комманды: %w", err)
+		return fmt.Errorf("Ошибка отправки INSTREAM команды: %w", err)
 	}
 
 	file, err := os.Open(filePath)
 	if err != nil {
-		return fmt.Errorf("Ошибка открытия файла: %w", err)
+		return fmt.Errorf("Ощибка открытия файла: %w", err)
 	}
 	defer file.Close()
 
@@ -54,10 +60,11 @@ func (s *Scanner) ScanFile(filePath string) error {
 				byte(n),
 			}
 			if _, err := conn.Write(sizeBuf); err != nil {
-				return fmt.Errorf("Ошибка записи чанка: %w", err)
+				return fmt.Errorf("Ошибка чтения размера чанка: %w", err)
 			}
+
 			if _, err := conn.Write(buf[:n]); err != nil {
-				return fmt.Errorf("Ошибка записи чанка: %w", err)
+				return fmt.Errorf("Ошибка записи даты чанка: %w", err)
 			}
 		}
 		if err == io.EOF {
@@ -70,26 +77,25 @@ func (s *Scanner) ScanFile(filePath string) error {
 
 	zero := []byte{0, 0, 0, 0}
 	if _, err := conn.Write(zero); err != nil {
-		return fmt.Errorf("Ошибка записи нулевого чанка: %w", err)
+		return fmt.Errorf("Ощибка записи нулевого чанка: %w", err)
 	}
 
 	response, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
-		return fmt.Errorf("ошибка чтения ответа: %w", err)
+		return fmt.Errorf("Ошибка чтения ответа: %w", err)
 	}
 
 	if response == "" {
-		return fmt.Errorf("Ошибка ответа от сервиса антивируса")
+		return fmt.Errorf("Пустой ответ от сервиса антивируса")
 	}
 
 	if responseContainsVirus(response) {
-		return fmt.Errorf("Файл заражен: %s", response)
+		return fmt.Errorf("ФАЙЛ ЗАРАЖЕН: %s", response)
 	}
 
 	return nil
-
 }
 
 func responseContainsVirus(response string) bool {
-	return response != "OK\n" && response != "stream: OK\n"
+	return response != "ОК\n" && response != "stream: OK\n"
 }
