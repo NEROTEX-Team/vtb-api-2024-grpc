9cb9ad78015dbbc7cfef916acd45c0e7d27a336a
Danila Kutuzov <danilakutuzov@gmail.com>
2024-11-20 23:58:28 +0300

    Add templates of keycloack client

--- internal/adapters/keycloak/client.go
+++ internal/adapters/keycloak/client.go
@@ -1,1 +1,29 @@
 package keycloak
+
+import (
+	"context"
+
+	"github.com/coreos/go-oicd"
+)
+
+type KeycloakConfig struct {
+	IssuerURL string
+	ClientID string
+}
+
+type KeycloakClient struct {
+	verifier *oicd.IDTokenVerifier
+}
+
+func NewKeycloakClient(ctx context.Context, config KeycloakConfig) (*KeycloakClient, error) {
+	verifier, err := oicd.NewIDTokenVerifier(ctx, config.IssuerURL, config.ClientID)
+	if err != nil {
+		return nil, err
+	}
+	return &KeycloakClient{verifier: verifier}, nil
+}
+
+func (c *KeycloakClient) VerifyIDToken(ctx context.Context, token string) (*oicd.IDToken, error) {
+	return c.verifier.Verify(ctx, token)
+}
+
