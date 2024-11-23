e8a4e2e182f6529fbf5c942890186bc68e862f03
Sergey Natalenko <sergey.natalenko@mail.ru>
2024-11-23 10:40:10 +0300

    Update docker-compose.yaml

--- internal/adapters/database/repository/user/repository_test.go
+++ internal/adapters/database/repository/user/repository_test.go
@@ -27,10 +27,10 @@ func InitializeSchema(t *testing.T, pool *pgxpool.Pool) {
 	_, err := pool.Exec(context.Background(), `
 		CREATE TABLE IF NOT EXISTS users (
 			id UUID PRIMARY KEY,
+			email VARCHAR(255) NOT NULL UNIQUE,
 			hashed_password VARCHAR(255) NOT NULL,
 			first_name VARCHAR(255) NOT NULL,
 			last_name VARCHAR(255) NOT NULL,
-			email VARCHAR(255) NOT NULL UNIQUE,
 			created_at TIMESTAMP NOT NULL DEFAULT TIMEZONE('utc', now()),
 			updated_at TIMESTAMP NOT NULL DEFAULT TIMEZONE('utc', now()),
 			deleted_at TIMESTAMP
